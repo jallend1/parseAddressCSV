78b3697677df3212084f17062af79267764579a8
Jason Allen <jallend1@gmail.com>
2025-03-11 17:53:18 -0700

    Parses sample CSV file for the sample addresses

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const fs = require("fs");
-fs.readFile("sampleAddresses.csv", "utf8", (err, data) => {
-  if (err) console.log(err);
-  else console.log(data);
-});
+import { parse } from "csv-parse";
+import { createReadStream } from "fs";
+
+const APIKEY = null;
+const APIURL = `https://maps.googleapis.com/maps/api/geocode/json`;
+
+const inputCSVFile = "sampleAddresses.csv";
+const outputCSVFile = "sampleAddressesWithCoordinates.csv";
+
+async function getAddresses() {
+  const addresses = [];
+  createReadStream(inputCSVFile)
+    .pipe(parse({ delimiter: ",", columns: true }))
+    .on("data", (data) => {
+      addresses.push(data["Recipient Address"]);
+    })
+    .on("end", () => {
+      console.log(addresses);
+      console.log("CSV file successfully processed");
+    });
+}
+
+getAddresses();
