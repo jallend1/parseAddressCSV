28bbf06e39f6f941d3982e685aee608f1917938c
Jason Allen <jallend1@gmail.com>
2025-03-12 16:39:50 -0700

    Adds storage of location name alongside address to location object

--- index.js
+++ index.js
@@ -4,36 +4,44 @@ import { parse } from "csv-parse";
 
 dotenv.config();
 const APIKEY = process.env.API_KEY;
-console.log(APIKEY);
-
 const APIURL = `https://maps.googleapis.com/maps/api/geocode/json?key=${APIKEY}`;
 const sampleAddress = "1600 Amphitheatre Parkway, Mountain View, CA";
 const inputCSVFile = "sampleAddresses.csv";
 const outputCSVFile = "sampleAddressesWithCoordinates.csv";
 
-const formatAddress = (address) => address.split(" ").join("%20");
+// const formatAddress = (address) => address.split(" ").join("%20");
 
-const fetchAddressCoordinates = async (address) => {
-  const formattedAddress = formatAddress(address);
-  const response = await fetch(`${APIURL}&address=${formattedAddress}`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch address coordinates");
-  }
-  const data = await response.json();
-  return data.results[0].geometry.location;
-};
+// const fetchAddressCoordinates = async (address) => {
+//   const formattedAddress = formatAddress(address);
+//   const response = await fetch(`${APIURL}&address=${formattedAddress}`);
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch address coordinates");
+//   }
+//   const data = await response.json();
+//   return data.results[0].geometry.location;
+// };
 
-fetchAddressCoordinates(sampleAddress).then((data) => {
-  console.log(data);
-  // Sample return: { lat: 37.4220113, lng: -122.0847483 }
-});
+// fetchAddressCoordinates(sampleAddress).then((data) => {
+//   console.log(data);
+//   // Sample return: { lat: 37.4220113, lng: -122.0847483 }
+// });
 
 async function getAddresses() {
   const addresses = [];
   createReadStream(inputCSVFile)
-    .pipe(parse({ delimiter: ",", columns: true }))
+    .pipe(
+      parse({
+        delimiter: ",",
+        columns: (header) => header.map((column) => column.trim()), // Recipient Company not working as a key without trim
+      })
+    )
     .on("data", (data) => {
-      addresses.push(data["Recipient Address"]);
+      const location = {
+        name: data["Recipient Company"],
+        address: data["Recipient Address"],
+      };
+
+      addresses.push(location);
     })
     .on("end", () => {
       console.log(addresses);
@@ -41,4 +49,4 @@ async function getAddresses() {
     });
 }
 
-// getAddresses();
+getAddresses();
